70b5aaaf6a569ce761c0e4f2909d153287e5dc81
Kamal Sharma <kamaldgrt@gmail.com>
2020-08-03 20:02:11 +0530

    Create 06_constant_variables.dart

---
+++ 06_constant_variables.dart
@@ -0,0 +1,13 @@
+main() {
+  const aConstNum = 0;
+  const aConstBool = true;
+  const aConstString = 'a constant string';
+
+  print(aConstNum);
+  print(aConstBool);
+  print(aConstString);
+
+  print(aConstNum.runtimeType);
+  print(aConstBool.runtimeType);
+  print(aConstString.runtimeType);
+}
