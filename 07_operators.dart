685288dec01c62620a59f9531baa445ac4f23f37
Kamal Sharma <kamaldgrt@gmail.com>
2020-08-03 23:22:01 +0530

    Create 07_operators.dart

---
+++ 07_operators.dart
@@ -0,0 +1,36 @@
+main() {
+  int num = 10 + 22;
+  num = num - 2;
+
+  print(num);
+
+  num = num % 5;
+  print(num);
+
+  // relational ==, !=, >=, <=
+  if (num == 0) {
+    print('Zero');
+  }
+
+  num = 100;
+  num *= 2;
+  print(num);
+
+  // Unary operator
+  ++num;
+  num++;
+  num += 1;
+  num -= 1;
+  print(num);
+
+  // Logical operators : &&, || , !=
+  if (num > 200 && num < 203) {
+    print('200 to 202');
+  }
+
+  // != Not Equal
+
+  if (num != 100) {
+    print('num is not equal to 100');
+  }
+}
